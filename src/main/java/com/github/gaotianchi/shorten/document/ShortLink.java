3bc6afe71e2526b5a6eb6975302d0a52b19c4f98
gaotianchi <6159984@gmail.com>
2025-03-31 15:48:26 +0800

    更新 application-v1.0.yml，启用 MongoDB 自动索引创建；更新 ShortLink.java，合并 ID 和 shortCode 字段的声明，并为 shortCode 添加唯一索引

--- src/main/java/com/github/gaotianchi/shorten/document/ShortLink.java
+++ src/main/java/com/github/gaotianchi/shorten/document/ShortLink.java
@@ -3,19 +3,18 @@ package com.github.gaotianchi.shorten.document;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/**
- * @author gaotianchi
- * @since 2025/3/22 下午6:05
- **/
 @Data
 @Builder
 @Document("short_link")
 public class ShortLink {
-    @Id
-    private String id;
+    @Id private String id;
+
+    @Indexed(unique = true) private String shortCode;
+
     private String originalUrl;
-    private String shortCode;
+
     private long expirationDate;
 }
