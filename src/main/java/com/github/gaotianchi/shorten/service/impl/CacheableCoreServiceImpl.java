58a41b91d7c0cd7af16860efc8c94e652a435232
gaotianchi <6159984@gmail.com>
2025-04-06 10:48:15 +0800

    实现 Caffeine 缓存集成，优化 URL 获取和删除逻辑，支持从本地内存、Redis 和数据库中获取数据

--- src/main/java/com/github/gaotianchi/shorten/service/impl/CacheableCoreServiceImpl.java
+++ src/main/java/com/github/gaotianchi/shorten/service/impl/CacheableCoreServiceImpl.java
@@ -1,6 +1,9 @@
 package com.github.gaotianchi.shorten.service.impl;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.gaotianchi.shorten.document.ShortLink;
 import com.github.gaotianchi.shorten.exception.GlobalIdException;
+import com.github.gaotianchi.shorten.repository.ShortLinkRepository;
 import com.github.gaotianchi.shorten.service.CoreService;
 import com.github.gaotianchi.shorten.utils.Base62Converter;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,11 +18,17 @@ import org.springframework.stereotype.Service;
 public class CacheableCoreServiceImpl implements CoreService {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final Cache<String, String> caffeineCache;
+    private final ShortLinkRepository shortLinkRepository;
 
     public CacheableCoreServiceImpl(
-            RedisTemplate<String, String> redisTemplate
+            RedisTemplate<String, String> redisTemplate,
+            Cache<String, String> caffeineCache,
+            ShortLinkRepository shortLinkRepository
     ) {
         this.redisTemplate = redisTemplate;
+        this.caffeineCache = caffeineCache;
+        this.shortLinkRepository = shortLinkRepository;
     }
 
     @Override
@@ -34,10 +43,43 @@ public class CacheableCoreServiceImpl implements CoreService {
 
     @Override
     public String getOriginalUrl(String shortCode) {
-        return null;
+        // 1. 优先从本地内存中获取数据
+        String originalUrl = caffeineCache.getIfPresent(shortCode);
+        if (originalUrl != null) {
+            return originalUrl;
+        }
+
+        // 2. 如果本地内存中没有，则从Redis中获取数据
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        originalUrl = ops.get(shortCode);
+        if (originalUrl != null) {
+            // 如果Redis中有，则将数据放入本地内存中
+            caffeineCache.put(shortCode, originalUrl);
+            return originalUrl;
+        }
+
+        // 3. 如果Redis中也没有，则从数据库中获取数据
+        ShortLink shortLink = shortLinkRepository.findByShortCode(shortCode);
+        if (shortLink == null) {
+            return null;
+        }
+        originalUrl = shortLink.getOriginalUrl();
+        // 如果数据库中有，则将数据放入Redis和本地内存中
+        ops.set(shortCode, originalUrl);
+        caffeineCache.put(shortCode, originalUrl);
+        return originalUrl;
     }
 
     @Override
     public void deleteLink(String shortCode) {
+
+        // 1. 从本地内存中删除数据
+        caffeineCache.invalidate(shortCode);
+
+        // 2. 从Redis中删除数据
+        redisTemplate.delete(shortCode);
+
+        // 3. 从数据库中删除数据
+        shortLinkRepository.deleteShortLinkByShortCode(shortCode);
     }
 }
